3d1cab4bc1f6a1ad59336c0c32ad780f4af325b6
Ronit Roushan <ronit21102@gmail.com>
2024-04-30 23:55:31 +0530

    initial

---
+++ xdictionary/src/App.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import meanings from './assets/data';
+
+const App = () => {
+	const [search, setsearch] = useState('');
+	const [found, setfound] = useState('');
+
+	const handleInputChange = (e ) => {
+		setsearch(e.target.value);
+	};
+
+	const handleClick = () => {
+		const allWords = meanings.find((meaning) => {
+			return meaning.word.toLowerCase() == search.toLowerCase();
+		});
+
+		if (allWords) {
+			setfound(allWords?.meaning);
+		} else {
+			setfound('notFound');
+		}
+
+		setsearch('');
+	};
+
+	return (
+		<div>
+			<h1>Dictionary App</h1>
+			<div className='search'>
+				<input
+					type='text'
+					placeholder='Search for a word...'
+					onChange={handleInputChange}
+					value={search}
+				/>
+				<button onClick={handleClick}>Search</button>
+			</div>
+			<h4>Definition:</h4>
+			{found !== '' && (
+				<>
+					{found === 'notFound' ? (
+						<h2>Word not found in the dictionary.</h2>
+					) : (
+						<h2>{found}</h2>
+					)}
+				</>
+			)}
+		</div>
+	);
+};
+
+export default App;
